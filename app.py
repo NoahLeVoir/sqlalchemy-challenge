1068220394723f0c71d19fd01a5b8adf746604fa
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-03-20 10:37:53 -0500

    Set up flask, and created welcome routes list

--- app.py
+++ app.py
@@ -28,3 +28,46 @@ Station = Base.classes.station
 
 # Create Session (Link) From Python to the DB
 session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+# Route creation
+# Home Route
+@app.route("/")
+def welcome():
+    return """<html>
+    <h1>Hawaii Climate App (Flask API)</h1>
+        <p>Precipitation Analysis:</p>
+            <ul>
+                <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
+            </ul>
+        <p>Station Analysis:</p>
+            <ul>
+                <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
+            </ul>
+        <p>Temperature Analysis:</p>
+            <ul>
+                <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
+            </ul>
+        <p>Start Day Analysis:</p>
+            <ul>
+                <li><a href="/api/v1.0/2017-03-14">/api/v1.0/2017-03-14</a></li>
+            </ul>
+        <p>Start & End Day Analysis:</p>
+            <ul>
+                <li><a href="/api/v1.0/2017-03-14/2017-03-28">/api/v1.0/2017-03-14/2017-03-28</a></li>
+            </ul>
+    </html>
+    """
+
+
+
+
+
+
+# Must have at end of flask
+if __name__ == '__main__':
+    app.run(debug = True)
