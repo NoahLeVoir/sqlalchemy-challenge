3bd4bdc8e525dd18acd0b34cd081b69a29c57a06
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-03-20 10:22:37 -0500

    Database setup and imports

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+# Import based on previous climate analysis
+import numpy as np
+import datetime as dt
+
+# Import SQLAlchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
+
+# Import Flask
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup (Same as before)
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect Existing Database Into a New Model
+Base = automap_base()
+# Reflect the Tables
+Base.prepare(engine, reflect = True)
+
+# Save References to Each Table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session (Link) From Python to the DB
+session = Session(engine)
